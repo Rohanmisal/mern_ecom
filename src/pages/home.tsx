7b59b3a84f610d5f350fdd8abe8d4c3ca7370af7
Rohan <rohanmisal89@gmail.com>
2024-02-25 20:40:07 +0530

    card created

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom"
+import ProductCard from "./product-card"
 
 const Home = () => {
+  const addToCartHandler= () => {};
   return (
-    <div>
-      HEllo
+    <div className="home">
+      <section></section>
+
+      <h1>Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard 
+        productId="adasd" 
+        name="mackbook"
+        price={4545}
+        stock={435}
+        handler={addToCartHandler}
+        photo="https://m.media-amazon.com/images/I/719C6bJv8jL._SX466_.jpg"
+        />
+      </main>
     </div>
   )
 }
