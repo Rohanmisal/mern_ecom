8246fc67758e5892d3af762f4124391aedafbf5c
Rohan <rohanmisal89@gmail.com>
2024-02-25 12:21:39 +0530

    pages Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/home"
+import Search from "./pages/search"
+import Cart from "./pages/cart"
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+   <Router>
+    {/* Header */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Search/>}/>
+        <Route path="/" element={<Cart/>}/>
+      </Routes>
+   </Router>
   )
 }
 
