bbb596020ba4cf3139b0333da35ef6c45976468e
Rohan <rohanmisal89@gmail.com>
2024-03-04 14:55:23 +0530

    first commit

--- ecom-backend/src/app.ts
+++ ecom-backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
+// Importing Routes
+import userRoute from "./routes/user.js"
+
 const port = 3000;
 const app = express();
 
-
-
-
+// Using Routes
+app.use("/api/v1/user", userRoute)
 
 app.listen(port, () => {
     console.log(`server is working on http://localhost:${port}`);
